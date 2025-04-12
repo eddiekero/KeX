23cfdd7adb6948029217c4aeaeef5c2a6ec4489d
Eddie Kero <eddie.k.a.kero@gmail.com>
2025-04-12 17:46:02 +0200

    progress

--- newtrain.py
+++ newtrain.py
@@ -5,52 +5,85 @@ import plyextract
 import shutil
 
 
-checkpoint = 0
-iteration_step = 100
-max_iteration = 500
 
-count_and_psnr = []
+iteration_step = 500
+max_iteration = 1000
 
 
-subprocess.run([sys.executable,
-                    "train.py", 
-                    "--model_path", "output/whatever0",
-                    "-s", "tandt_db\\tandt\\truck\\",
-                    "--eval",
-                    "--iterations", "100", 
-                    "--checkpoint_iterations", "100",])
-checkpoint+=1
+count_and_psnr = {}
 
-for iter in range(100, max_iteration, iteration_step):
+scenes = [
+    "tandt_db/tandt/truck/",
+    "tandt_db/tandt/train",
+]
+
+def InitTrainingRun():
     subprocess.run([sys.executable,
-                    "train.py", 
-                    "--model_path", "output/whatever%d" % (checkpoint),
-                    "-s", "tandt_db\\tandt\\truck\\",
-                    "--eval",
-                    "--start_checkpoint", "output/whatever%d/chkpnt%d.pth" % (checkpoint-1, iter),
-                    "--iterations", "%d" % (iter+100), 
-                    "--checkpoint_iterations", "%d" % (iter+100),])
-    
-    shutil.rmtree("output/whatever%d/" % (checkpoint-1))
+                        "train.py", 
+                        "--model_path", "output/whatever0",
+                        "-s", scene_dir,
+                        "--eval",
+                        "--iterations", f"{iteration_step}", 
+                        "--checkpoint_iterations", f"{iteration_step}",])
     
     subprocess.run([sys.executable,
-                    "render.py", 
-                    "-m", "output/whatever%d" % checkpoint,
-                    "--skip_train",])
-    
+                        "render.py", 
+                        "-m", "output/whatever0",
+                        "--skip_train",])
+        
     subprocess.run([sys.executable,
                     "metrics.py", 
-                    "-m", "output/whatever%d" % checkpoint,])
+                    "-m", "output/whatever0",])
     
-    with open("output/whatever%d/results.json" % checkpoint, "r") as file:
+    with open("output/whatever0/results.json", "r") as file:
         data = json.load(file)
 
-    psnr = data["ours_%d" % (iter+100)]["PSNR"]
+    psnr = data["ours_%d" % (iteration_step)]["PSNR"]
 
-    gaussian_count = plyextract.get_vertex_count("output/whatever%d/point_cloud/iteration_%d/point_cloud.ply" % (checkpoint, iter+100))
+    gaussian_count = plyextract.get_vertex_count("output/whatever0/point_cloud/iteration_%d/point_cloud.ply" % (iteration_step))
 
-    count_and_psnr.append((gaussian_count, psnr))
+    count_and_psnr[scene_dir].append((gaussian_count, psnr))
+    return
 
-    checkpoint+=1
+for scene_dir in scenes:
+    
+    count_and_psnr[scene_dir] = []
 
-print(count_and_psnr)
+    InitTrainingRun()
+    checkpoint = 1
+
+    for iter in range(iteration_step, max_iteration, iteration_step):
+        subprocess.run([sys.executable,
+                        "train.py", 
+                        "--model_path", "output/whatever%d" % (checkpoint),
+                        "-s", scene_dir,
+                        "--eval",
+                        "--start_checkpoint", "output/whatever%d/chkpnt%d.pth" % (checkpoint-1, iter),
+                        "--iterations", "%d" % (iter+iteration_step), 
+                        "--checkpoint_iterations", "%d" % (iter+iteration_step),])
+        
+        shutil.rmtree("output/whatever%d/" % (checkpoint-1))
+        
+        subprocess.run([sys.executable,
+                        "render.py", 
+                        "-m", "output/whatever%d" % checkpoint,
+                        "--skip_train",])
+        
+        subprocess.run([sys.executable,
+                        "metrics.py", 
+                        "-m", "output/whatever%d" % checkpoint,])
+        
+        with open("output/whatever%d/results.json" % checkpoint, "r") as file:
+            data = json.load(file)
+
+        psnr = data["ours_%d" % (iter+iteration_step)]["PSNR"]
+
+        gaussian_count = plyextract.get_vertex_count("output/whatever%d/point_cloud/iteration_%d/point_cloud.ply" % (checkpoint, iter+iteration_step))
+
+        count_and_psnr[scene_dir].append((gaussian_count, psnr))
+
+        checkpoint+=1
+
+    print(f"{scene_dir}: count_and_psnr = {count_and_psnr[scene_dir]}")
+    shutil.rmtree("output/whatever%d/" % (checkpoint-1))
+
